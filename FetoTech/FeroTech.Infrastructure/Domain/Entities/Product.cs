ac123d29d41c786906a09e5f7742a7102daafe32
Ariful Islam Akash <ariful.akash23@gmail.com>
2025-10-28 09:56:28 +0600

    published

---
+++ FetoTech/FeroTech.Infrastructure/Domain/Entities/Product.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FeroTech.Infrastructure.Domain.Entities
+{
+    public class Product
+    {
+        [Key]
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public decimal Price { get; set; }
+    }
+}
---
+++ FetoTech/FeroTech.Web/Controllers/ProductController.cs
@@ -0,0 +1,49 @@
+﻿using FeroTech.Infrastructure.Application.DTOs;
+using FeroTech.Infrastructure.Application.Interfaces;
+using FeroTech.Infrastructure.Domain.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FeroTech.Web.Controllers
+{
+
+    public class ProductController : Controller
+    {
+        private readonly IProductRepository _repo;
+
+        public ProductController(IProductRepository repo)
+        {
+            _repo = repo;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var products = await _repo.GetAllAsync();
+            var dtos = products.Select(p => new ProductDto
+            {
+                Id = p.Id,
+                Name = p.Name,
+                Price = p.Price
+            });
+            return View(dtos);
+        }
+
+        [HttpGet]
+        public IActionResult Create() => View();
+
+        [HttpPost]
+        public async Task<IActionResult> Create(ProductDto dto)
+        {
+            if (ModelState.IsValid)
+            {
+                var product = new Product
+                {
+                    Name = dto.Name,
+                    Price = dto.Price
+                };
+                await _repo.AddAsync(product);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(dto);
+        }
+    }
+}
